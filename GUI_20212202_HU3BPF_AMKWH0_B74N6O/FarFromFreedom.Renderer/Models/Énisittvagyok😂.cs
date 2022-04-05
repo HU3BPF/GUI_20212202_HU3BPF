61bc4ef3c4cad15d34ae5ac54bc1660f758a0b1d
bekabalogh <balogh.rebeka@stud.uni-obuda.hu>
2022-04-05 21:54:23 +0200

    Models mappa

---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom.Renderer/Models/Énisittvagyok😂.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarFromFreedom.Renderer.Models
+{
+    internal class Énisittvagyok__
+    {
+    }
+}
