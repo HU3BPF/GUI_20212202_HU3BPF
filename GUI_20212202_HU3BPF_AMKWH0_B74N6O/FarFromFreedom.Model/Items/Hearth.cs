15fe84d9e0ff8295837535ed7f9ee12ad29229bb
DobiaszMartin <HU3BPF@stud.uni-obuda.hu>
2022-04-07 21:26:40 +0200

    Back

---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom.Model/Items/Hearth.cs
@@ -0,0 +1,21 @@
+﻿namespace FarFromFreedom.Model.Items
+{
+    public class Hearth : IItem
+    {
+        public Hearth(string name, string description, double health)
+        {
+            this.name = name;
+            this.description = description;
+            this.health = health;
+        }
+
+        private string name;
+        private string description;
+        private double health;
+
+
+        public string Name => name;
+        public string Description => description;
+        public double Health => health;
+    }
+}
---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom.Model/Items/Star.cs
@@ -0,0 +1,21 @@
+﻿namespace FarFromFreedom.Model.Items
+{
+    public class Star : IItem
+    {
+        public Star(string name, string description, double power)
+        {
+            this.name = name;
+            this.description = description;
+            this.power = power;
+        }
+
+        private string name;
+        private string description;
+        private double power;
+
+
+        public string Name => name;
+        public string Description => description;
+        public double Power => power;
+    }
+}
