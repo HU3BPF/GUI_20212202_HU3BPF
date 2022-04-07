9dc451f481ae45099feca848754b697eb90a12b9
bekabalogh <balogh.rebeka@stud.uni-obuda.hu>
2022-04-07 22:48:28 +0200

    LoadBackground

---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FarFromFreedom.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FarFromFreedom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainMenu page=new MainMenu();
+            page.Show();
+        }
+    }
+}
---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom/Pages/MainMenu.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarFromFreedom.Pages
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainMenu"/> class.
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+          
+
+            BitmapImage bmp = new BitmapImage(new Uri(System.IO.Path.Combine("Pages", "FFF_Menu.jpg"), UriKind.RelativeOrAbsolute));
+            //bmp.BeginInit();
+            //bmp.StreamSource = Assembly.GetExecutingAssembly().GetManifestResourceStream("FarFromFreedom.Pages.Resources.FFF_Menu.jpg");
+            //bmp.EndInit();
+            ImageBrush ib = new ImageBrush(bmp);
+            this.Background = ib;
+        }
+    }
+}
