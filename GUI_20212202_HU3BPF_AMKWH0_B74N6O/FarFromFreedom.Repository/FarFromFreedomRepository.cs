f0d8602dceff41773e01585c3df4c960caca619e
DobiaszMartin <HU3BPF@stud.uni-obuda.hu>
2022-04-07 21:36:56 +0200

    Repository Created

---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom.Repository/FarFromFreedomRepository.cs
@@ -0,0 +1,47 @@
+﻿using FarFromFreedom.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarFromFreedom.Repository
+{
+    public class FarFromFreedomRepository : IFarFromFreedomRepository<IGameModel, SavedGame>
+    {
+        public IGameModel LoadGame(string filename)
+        {
+            IGameModel gameModel;
+
+            using (StreamReader r = new StreamReader($"{filename}.json"))
+            {
+                string json = r.ReadToEnd();
+
+                gameModel = JsonConvert.DeserializeObject<IGameModel>(json, new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.None,
+                    MissingMemberHandling = MissingMemberHandling.Ignore,
+                });
+            }
+
+            return gameModel;
+        }
+
+        public List<SavedGame> LoadHighScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveGame(IGameModel gameModel, string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveHighScore(SavedGame highscore)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom.Repository/IFarFromFreedomRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarFromFreedom.Repository
+{
+    public interface IFarFromFreedomRepository<TGameModel, THighScore>
+    {
+        TGameModel LoadGame(string filename);
+        void SaveGame(TGameModel gameModel, string filename);
+        List<THighScore> LoadHighScore();
+        void SaveHighScore(THighScore highscore);
+    }
+}
