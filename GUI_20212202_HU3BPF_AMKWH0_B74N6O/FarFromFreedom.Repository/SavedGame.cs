c463363022ad14ab484a853cc2f18ab16104f827
DobiaszMartin <HU3BPF@stud.uni-obuda.hu>
2022-04-07 22:19:06 +0200

    Save Game added

---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom.Repository/SavedGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FarFromFreedom.Repository
+{
+    public class SavedGame
+    { 
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+        public DateTime date { get; set; }
+
+        public int MapLevel { get; set; }
+    }
+}
