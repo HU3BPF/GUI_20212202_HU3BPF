0506e34d035a34148d5e409eb02939b1a77e5589
DobiaszMartin <HU3BPF@stud.uni-obuda.hu>
2022-04-10 15:22:30 +0200

    Class Libary changed to wpf Class Libary

---
+++ GUI_20212202_HU3BPF_AMKWH0_B74N6O/FarFromFreedom/GameItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media;
+
+namespace FarFromFreedom
+{
+    public abstract class GameItem
+    {
+        public abstract Geometry Area { get; }
+
+        public bool IsCollision(GameItem other)
+        {
+            return Geometry.Combine(this.Area, other.Area,
+                GeometryCombineMode.Intersect, null).GetArea() > 0;
+        }
+    }
+}
